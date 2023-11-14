fbb00e7e6eb6c239342f5dc0aaa5d25fb13cbc9a
StanM84 <stan1984@abv.bg>
2023-11-14 16:09:05 +0200

    Add project files.

---
+++ 1.CountRealNumbers/1.CountRealNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+
+int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+SortedDictionary<int, int> numbersFriqency = new();
+
+foreach (int number in numbers)
+{
+    if (numbersFriqency.ContainsKey(number))
+    {
+        numbersFriqency[number] += 1;
+    }
+    else
+    {
+        numbersFriqency.Add(number, 1);
+    }
+}
+foreach (KeyValuePair<int, int> pair in numbersFriqency)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
